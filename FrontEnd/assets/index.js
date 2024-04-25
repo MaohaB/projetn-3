3e657ff524ff7a24d344e65f14598f15d0c63aa3
MaohaB <leila.posonsby@gmail.com>
2024-04-25 21:25:54 +0200

    récupération des ID quand on clique

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,55 +1,67 @@
-// works //
-fetch("http://localhost:5678/api/works")
-.then(function(response) {
-	if(response.ok) {
-		return response.json();
-	}
-})
-.then(function(data) {
-	let works = data;
-	console.log(works);
-	works.forEach((work, index) => {
-		// figure //
-		const Figure = document.createElement('figure');
-		Figure.setAttribute('class', `work-item category-id-0 category-id-${work.categoryId}`);
-		Figure.setAttribute('id', `work-item-${work.id}`);
-		// image //
-		const Img = document.createElement('img');
-		Img.setAttribute('src', work.imageUrl);
-		Img.setAttribute('alt', work.title);
-		Figure.appendChild(Img);
-		// figcaption //
-		const FigCaption = document.createElement('figcaption');
-		FigCaption.textContent = work.title;
-		Figure.appendChild(FigCaption);
+// travaux//
 
-		document.querySelector("div.gallery").appendChild(Figure);
-	});
-})
-.catch(function(err) {
-	console.log(err);
-});
-
+function getWorks() {
+	fetch("http://localhost:5678/api/works")
+	  .then(function (response) {
+		if (response.ok) {
+		  return response.json();
+		}
+	  })
+	  .then(function (data) {
+		let works = data;
+		console.log(works);
+		works.forEach((work, index) => {
+		  // figure //
+		  const Figure = document.createElement("figure");
+		  Figure.setAttribute("class",`work-item category-id-${work.categoryId}`);
+		  Figure.setAttribute("id", `work-item-${work.id}`);
+		  // image //
+		  const Img = document.createElement("img");
+		  Img.setAttribute("src", work.imageUrl);
+		  Img.setAttribute("alt", work.title);
+		  Figure.appendChild(Img);
+		  // figcaption //
+		  const FigCaption = document.createElement("figcaption");
+		  FigCaption.textContent = work.title;
+		  Figure.appendChild(FigCaption);
+  
+		  document.querySelector("div.gallery").appendChild(Figure);
+		});
+	  });
+  }
+  getWorks();
 
 
 // boutons //
 fetch("http://localhost:5678/api/categories")
-.then(function(response) {
-	if(response.ok) {
-		return response.json();
-	}
-})
-.then(function(data) {
-	let categories = data;
-	categories.unshift({id: 0, name: "Tous"});
+  .then(function (response) {
+    if (response.ok) {
+      return response.json();
+    }
+  })
+  .then(function (data) {
+    let categories = data;
+    categories.unshift({ id: 0, name: "Tous" });
 
-	console.log(categories);
+    console.log(categories);
 
-	const bouton = document.getElementById('boutons');
+    const bouton = document.getElementById("boutons");
+	console.log(bouton);
+    categories.forEach((category) => {
+      const btn = document.createElement("button");
+      btn.textContent = category.name;
+	  btn.id = category.id;
+      bouton.appendChild(btn);
+    });
+// filtres //
+    const buttons = document.querySelectorAll("button")
+	buttons.forEach(buttons => {
+		buttons.addEventListener("click",(e) => {
+			console.log(e.target.id)
+		});
 
-	categories.forEach((category) => {
-	  const btn = document.createElement('button');
-	  btn.textContent = category.name;
-	  bouton.appendChild(btn); });
-})
+	});
+	
+
+  })
 
